d2bd2e4171fdc76a914b470f70f03abc9a41bcd3
Solar-Subhranshu <solarsubhranshu@gmail.com>
2024-12-28 13:40:32 +0530

    some changes

--- Server/index.js
+++ Server/index.js
@@ -15,7 +15,10 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cookieParser());
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: true,
+    credentials:true
+}));
 app.use(express.urlencoded({ extended:true }));
 app.use("/auth", authEmpRoute);
 app.use("/company",companyRoute);
