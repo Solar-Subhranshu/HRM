bfbe55ec3cb627498d8eb5bee0a23df9a58c36e2
Solar-Subhranshu <solarsubhranshu@gmail.com>
2025-01-03 11:26:46 +0530

    some changes

--- Server/controller/employee/employee.controller.js
+++ Server/controller/employee/employee.controller.js
@@ -344,6 +344,7 @@ const deactivateEmp = async (req,res)=>{
 
 // done
 const showAllEmployee= async (req,res) =>{
+
     try{
         const allEmp = await Employee.find()
         .populate({
@@ -383,15 +384,17 @@ const showAllEmployee= async (req,res) =>{
                 message : "No Employee Found, Please Register Employee First"
             });
         }
+        
         else{
             return res.status(200).json({
                 success: true,
                 message: "List of All Employee",
-                root: "http://localhost:8000/",
+                root: `${process.env.SERVER_ADDRESS}${process.env.PORT}/`,
                 data : allEmp
 
             });
         }
+        
     }
     catch(error){
         return res.status(500).json({
