ea57dc2c399e70e0f2a5fe76fad65e2587f40ecc
Solar-Subhranshu <solarsubhranshu@gmail.com>
2025-01-04 11:29:46 +0530

    new changes

---
+++ Server/models/common/workType.model.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const workTypeSchema = new mongoose.Schema({
+    workType:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    created_By:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Employee"
+    },
+    updated_By:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Employee"
+    }
+},{timestamps:true});
+
+const WorkType = mongoose.model("WorkType",workTypeSchema);
+
+module.export = WorkType;
