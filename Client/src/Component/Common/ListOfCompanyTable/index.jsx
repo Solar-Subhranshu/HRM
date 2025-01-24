75818bf793879b2a525eaf7365fd068f9566a437
solarShivam <solarshivam9@gmail.com>
2025-01-24 14:34:36 +0530

    modify show company table

--- Client/src/Component/Common/ListOfCompanyTable/index.jsx
+++ Client/src/Component/Common/ListOfCompanyTable/index.jsx
@@ -1,3 +1,74 @@
+// import React, { useEffect, useState } from "react";
+// import { FaListUl } from "react-icons/fa6";
+// import axios from "axios";
+
+// function AddNewCompany() {
+//   const [showAllCompanyDetails, setAllCompanyDetails] = useState([]);
+
+//   const fetchAllCompanyDetails = async () => {
+//     try {
+//       const response = await axios.get('http://localhost:8000/common/show-all-companyDetails');
+//       setAllCompanyDetails(response.data.data);
+//       console.log(showAllCompanyDetails);
+
+//     } catch (error) {
+//       alert("Unable to  fetch all company data", error);
+//     }
+//   };
+
+  
+//   useEffect(() => {
+//     fetchAllCompanyDetails();
+//   }, []);
+
+//   return (
+//     <div className="ml-1 w-[calc(100%-30%)]">
+//       {/* Header Section */}
+//       <div className="bgMainColor flex py-4 pl-1 gap-3">
+//         <FaListUl size={24} />
+//         <h4 className="text-white">List of Company</h4>
+//       </div>
+
+//       {/* Table Section */}
+//       <div>
+//         <table className="table-auto w-full border border-black">
+//           <thead className="border border-black">
+//             <tr>
+//               {/* <th className="text-blue-600/100 border border-black w-16">Select</th> */}
+//               <th className="text-blue-600/100 border border-black">Company</th>
+//               <th className="text-blue-600/100 border border-black">Branch</th>
+//               <th className="text-blue-600/100 border border-black">Address</th>
+//               <th className="text-blue-600/100 border border-black">Pin</th>
+//             </tr>
+//           </thead>
+//           </table>
+          
+//           <div className="max-h-[200px] overflow-y-auto ">
+//           <table className="table-auto w-full border border-black">
+//           <tbody className="">
+//             {showAllCompanyDetails.map(({ _id, address, name, companyID,pin }) => (
+//               <tr key={_id}>
+//                 {/* <td className="border border-gray-300 flex justify-center items-center align-middle h-8 ">
+//                   <input type="checkbox" />
+//                 </td> */}
+//                 <td className="border border-black">{companyID?.name}</td>
+//                 <td className="border border-black">{name}</td>
+//                 <td className="border border-black">{address}</td>
+//                 <td className="border border-black">{pin}</td>
+//               </tr>
+//             ))}
+//           </tbody>
+//         </table>
+//         </div>
+
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default AddNewCompany;
+
+
 import React, { useEffect, useState } from "react";
 import { FaListUl } from "react-icons/fa6";
 import axios from "axios";
@@ -7,16 +78,16 @@ function AddNewCompany() {
 
   const fetchAllCompanyDetails = async () => {
     try {
-      const response = await axios.get('http://localhost:8000/common/show-all-companyDetails');
+      const response = await axios.get(
+        "http://localhost:8000/common/show-all-companyDetails"
+      );
       setAllCompanyDetails(response.data.data);
       console.log(showAllCompanyDetails);
-
     } catch (error) {
-      alert("Unable to  fetch all company data", error);
+      alert("Unable to fetch all company data", error);
     }
   };
 
-  
   useEffect(() => {
     fetchAllCompanyDetails();
   }, []);
@@ -26,47 +97,37 @@ function AddNewCompany() {
       {/* Header Section */}
       <div className="bgMainColor flex py-4 pl-1 gap-3">
         <FaListUl size={24} />
-        <h4 className="text-white">List of Company</h4>
+        <h4 className="text-white">Show all Company</h4>
       </div>
 
       {/* Table Section */}
-      <div>
-        <table className="table-auto w-full border border-black">
-          <thead className="border border-black">
+      <div className="max-h-[250px] overflow-y-auto border border-gray-500">
+        <table className="table-auto w-full border-collapse border border-gray-500">
+          <thead className="bg-gray-100 sticky top-0 z-10">
             <tr>
-              {/* <th className="text-blue-600/100 border border-black w-16">Select</th> */}
-              <th className="text-blue-600/100 border border-black">Company</th>
-              <th className="text-blue-600/100 border border-black">Branch</th>
-              <th className="text-blue-600/100 border border-black">Address</th>
-              <th className="text-blue-600/100 border border-black">Pin</th>
+              <th className="text-blue-600 border border-gray-500">Company</th>
+              <th className="text-blue-600 border border-gray-500">Branch</th>
+              <th className="text-blue-600 border border-gray-500">Address</th>
+              <th className="text-blue-600 border border-gray-500">Pin</th>
             </tr>
           </thead>
-          </table>
-          
-          <div className="max-h-[200px] overflow-y-auto ">
-          <table className="table-auto w-full border border-black">
-          <tbody className="">
-            {showAllCompanyDetails.map(({ _id, address, name, companyID,pin }) => (
-              <tr key={_id}>
-                {/* <td className="border border-gray-300 flex justify-center items-center align-middle h-8 ">
-                  <input type="checkbox" />
-                </td> */}
-                <td className="border border-black">{companyID?.name}</td>
-                <td className="border border-black">{name}</td>
-                <td className="border border-black">{address}</td>
-                <td className="border border-black">{pin}</td>
-              </tr>
-            ))}
+          <tbody>
+            {showAllCompanyDetails.map(
+              ({ _id, address, name, companyID, pin }) => (
+                <tr key={_id}>
+                  <td className="text-center border border-gray-500">{companyID?.name}</td>
+                  <td className="text-center border border-gray-500">{name}</td>
+                  <td className="text-center border border-gray-500">{address}</td>
+                  <td className="text-center border border-gray-500">{pin}</td>
+                </tr>
+              )
+            )}
           </tbody>
         </table>
-        </div>
-
       </div>
     </div>
   );
 }
 
 export default AddNewCompany;
-
-
 
